cd9a9e50fec228bbdb8140bf87a5112a7d937992
Wagaguya <60981745+pankourgiou@users.noreply.github.com>
2026-05-27 06:30:02 +0300

    Update and rename Pterodactyl.R to energy.swift

---
+++ energy.swift
@@ -0,0 +1,11 @@
+//setting :) = 1 we have:
+import Foundation
+
+let bpm = 70.0
+let beatDuration = 60.0 / bpm // seconds per beat
+
+while true {
+    print("E = :)*mc**2")
+    fflush(stdout)
+    Thread.sleep(forTimeInterval: beatDuration)
+}
